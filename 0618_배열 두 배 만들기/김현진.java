ec2c611f5610b82354036fffea2d671e887c7ead
김현진 <149029049+0515khj@users.noreply.github.com>
2024-06-19 09:01:54 +0900

    Create 김현진.java

---
+++ 0618_배열 두 배 만들기/김현진.java
@@ -0,0 +1,13 @@
+package ex01;
+
+public class testkhj {
+	
+	public static int[] khj(int[] num) {
+		int[] result = new int[num.length];
+		for (int i = 0; i < num.length; i++) {
+			result[i] = num[i] *2;
+		}
+		return result;
+	}
+
+}
