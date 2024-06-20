f50ab062d4a435d3557d785f5050e0650af953d0
김현진 <149029049+0515khj@users.noreply.github.com>
2024-06-20 23:58:45 +0900

    Update 김현진.java

--- 0620_5명씩/김현진.java
+++ 0620_5명씩/김현진.java
@@ -1,1 +1,13 @@
-
+class Solution {
+    public String[] solution(String[] names) {
+        int cnt = (names.length + 4) / 5;
+        String[] answer = new String[cnt];
+        int idx = 0;
+        for(int i =0;i<names.length;i++){
+            if(i % 5 ==0 ){
+            answer[idx++] = names[i];    
+            }
+        }
+        return answer;
+    }
+}
