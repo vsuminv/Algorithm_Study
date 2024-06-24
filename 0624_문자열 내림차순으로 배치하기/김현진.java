ac9c8bc813435e650856211f5dfa098ae7d9bbd1
김현진 <149029049+0515khj@users.noreply.github.com>
2024-06-24 23:20:51 +0900

    Update 김현진.java

--- 0624_문자열 내림차순으로 배치하기/김현진.java
+++ 0624_문자열 내림차순으로 배치하기/김현진.java
@@ -1,1 +1,15 @@
-1
+import java.util.*;
+
+class Solution {
+    public String solution(String s) {
+        String[] answer = s.split("");
+        Arrays.sort(answer, Collections.reverseOrder());
+        String khj = "";
+        for(String str : answer){
+            khj += str;
+        }
+        
+        
+        return khj;
+    }
+}
