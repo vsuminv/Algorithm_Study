696b41fcf90906b167f847813ccb7513f14f7edc
김현진 <149029049+0515khj@users.noreply.github.com>
2024-06-25 15:34:40 +0900

    Create 김현진.java

---
+++ 0625_Map실습/김현진.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class MapExam{
+    public Map<String, Integer> makeMap(){
+        Map<String, Integer> products = new HashMap<>();
+        //상품의 이름과 값을 products에 추가해 보세요
+        products.put("가위",2500);
+        products.put("크레파스",5000);
+        return products;
+    }
+    public static void main(String[] args){
+        MapExam me = new MapExam();
+        
+    }
+}
