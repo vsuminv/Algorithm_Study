b32f8f0731010901ea365b60e1740cf5e9fd2a64
zoown12 <96428476+zoown12@users.noreply.github.com>
2024-06-26 17:27:00 +0900

    0 떼기 /김동훈

---
+++ 0626_0 떼기/김동훈.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+class Solution {
+    public String solution(String n_str) {
+        String answer = "";
+                
+        answer = n_str.replaceFirst("^0+","");
+        
+        return answer;
+    }
+}
