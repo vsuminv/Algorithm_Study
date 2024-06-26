adff90e5bb1c99b835329ee067aac13d0469ed7e
김현진 <149029049+0515khj@users.noreply.github.com>
2024-06-26 22:45:25 +0900

    Update 김현진.java

--- 0626_0 떼기/김현진.java
+++ 0626_0 떼기/김현진.java
@@ -1,1 +1,9 @@
-1
+class Solution {
+    public String solution(String n_str) {
+        int idx = 0;
+        while(idx < n_str.length() && n_str.charAt(idx) == '0'){
+            idx++;
+        }
+        return n_str.substring(idx);
+    }
+}
