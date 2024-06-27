422be006a547ab9863b0beba0f7c5b549e52bb17
김현진 <149029049+0515khj@users.noreply.github.com>
2024-06-27 20:40:39 +0900

    Create 김현진.java

---
+++ 마지막 두 원소 /김현진.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int[] solution(int[] num_list) {
+        int[] answer = new int[num_list.length + 1];
+        for(int i =0;i<num_list.length; i++){
+            answer[i] = num_list[i];
+        }
+        int last = num_list.length - 1;
+        if(num_list[last] > num_list[last - 1]){
+            answer[answer.length - 1]=num_list[last] - num_list[last - 1];
+        }else{
+            answer[answer.length - 1] = num_list[last] * 2;
+        }
+        
+        return answer;
+    }
+}
