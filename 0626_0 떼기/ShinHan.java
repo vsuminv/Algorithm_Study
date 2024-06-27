cf84610f8d1db9809c72a95a967c1f50be4f90af
ruukr8080@gmail.com <ruukr8080@gmail.com>
2024-06-27 09:21:24 +0900

    0빼기/신한

---
+++ 0626_0 떼기/ShinHan.java
@@ -0,0 +1,10 @@
+package src;
+
+public class ShinHan {
+    class Solution {
+        public String solution(String n_str) {
+            int i = Integer.parseInt(n_str);
+            return i + "";
+        }
+    }
+}
